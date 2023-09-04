c5164d9407acee0a4f8812fd83030da616d20cf8
WasawatB <bun.wasawat@outlook.com>
2023-09-04 22:47:56 +0100

    resolve conflict

--- main.py
+++ main.py
@@ -25,7 +25,8 @@ if __name__ == '__main__':
     print('input images', img_paths)
     print('input fonts', font_paths)
 
-    out_img = img.process(img_paths, font_paths, config.ARTIST, config.SONG, config.EXTRACT_MODE, config.COLOR_MODE[1])
+    #out_img = img.process(img_paths, font_paths, config.ARTIST, config.SONG, config.EXTRACT_MODE, config.COLOR_MODE[1])
+    out_img = img.process(img_paths, font_paths, config.ARTIST, config.SONG, config.EXTRACT_MODE)
     out_img.show()
 
     w_dpi = 1200
